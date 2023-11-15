e1e2116f7280d3cf0ee7c745794f0f9b09ec7470
esquire-out <148168035+esquire-out@users.noreply.github.com>
2023-11-15 00:56:46 +0000

    Refactored a lot, please read comments in code.

--- Shuffler/Shuffler.py
+++ Shuffler/Shuffler.py
@@ -1,47 +1,60 @@
 import random
 
-def assign_tasks(task_file, title_file):
-    assigned = [] # List of people to be assigned tasks
-    unassigned = [] # List of people not assigned tasks
-    titles = [] # List of titles - shuffled and sorted
-    tasks = [] # List of tasks - shuffled
-    to_zip = ''
-    to_file = ''
-    to_print = ''
-    task_count = 0 # Number of tasks
-    
-    with open(task_file, 'r') as taskfile: # Opens task_file
-        for task in taskfile: # Iterates through each line
-            tasks += task.split('\n') # Splits list by new line
-            tasks = [x for x in tasks if x] # Removes empty strings
-            random.shuffle(tasks) # Shuffles tasks to prevent bias
-            task_count += 1 # Counts the tasks iterated through
-            continue
-    
-    with open(title_file, 'r') as titlefile: # Opens title_file
-        for title in titlefile: # Iterates through each line
-            titles += title.splitlines() # Splits list by new line
-            titles = [x for x in titles if x] # Removes empty strings
-            random.shuffle(titles) # Shuffles titles to prevent bias
-            titles.sort(reverse=True, key=lambda v: v.split()[-1]) # Orders titles by integer
-            continue
+def read_file_to_list(filename):
+    with open(filename, 'r') as file:
+        # Splits list by new line
+        file_list = file.read().splitlines()
+        # Remove empty strings from list
+        file_list = [x for x in file_list if x] 
+        return file_list
 
-        assigned += titles[:task_count] # Splits titles by who will be assigned
-        for person in assigned:
-            person = person[:-1] # Removes the integer
-            to_file += str(person) + '0\n' # Resets the integer to send to file
-            to_print += str(person)
-        
-        zipped_list = list(zip(assigned, tasks))
-        
-        unassigned += titles[task_count:]
-        for person in unassigned:
-            oldtime = person[-1]
-            newtime = int(oldtime) + 1
-            person = person[:-1] + str(newtime)
-            to_file += str(person) + '\n'
-            continue
+def shuffle_list(file_list):
+    # Shuffles list to prevent bias
+    random.shuffle(file_list)
+    return file_list
 
-    return zipped_list
+class Person:
+    # This method is called when the object is created
+    # It sets the attributes of the object
+    # Parameters:
+    #   name: The name of the person
+    #   Weight: The weight of the person (used for sorting, this is the priority)
+    #   task: The task of the person
+    def __init__(self, name, Weight, task):
+        self.name = name
+        self.Weight = Weight
+        self.task = task
+
+    # This method is called when the object is converted to a string
+    # It returns the string representation of the object
+    # This is useful for printing
+    def __str__(self):
+        return f'{self.name} {self.task}'
+
+    # This method is called when the object is printed
+    # It returns the string representation of the object
+    # This is useful for debugging
+    def __repr__(self):
+        return f'{self.name} {self.task}'
+
+def assign_task():
+    # Reads people.txt and converts it to a list, sort it by the last character (int) and reverse it
+    people_list = sorted(read_file_to_list('./Config/Titles.txt'), key=lambda x: int(x[-2:]), reverse=True)
     
-print(assign_tasks('Shuffler/Config/Tasks.txt', 'Shuffler/Config/Titles.txt')) # prints and assigns filenames
+    # Reads tasks.txt and converts it to a list
+    task_list = shuffle_list(read_file_to_list('./Config/Tasks.txt'))
+
+    # zip both shuffled lists together
+    zipped_list = zip(people_list, task_list)
+    for person, task in zipped_list:
+        # Create a person object
+        #                        name,       weight,    task
+        PersonObject = Person(person[:-1], person[-2:], task)
+        # Note that the field PersonObject.Weight is not used, it is still there.
+        print(f'Name: {PersonObject.name} |> Task: {PersonObject.task}')
+
+
+# This is the main function
+# It is called when the file is run
+if __name__ == '__main__':
+    assign_task()
